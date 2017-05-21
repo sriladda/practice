71a1a989ecaa7e1d34c04d980464a284dc84946c
Srilatha Addanki <contactsrilatha1@gmail.com>
2017-05-20 20:31:31 -0700

    bfs

---
+++ trees/bfs.c
@@ -0,0 +1,82 @@
+//http://www.geeksforgeeks.org/level-order-tree-traversal/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_ptr {
+   int data;
+   struct node_ptr *left; 
+   struct node_ptr *right; 
+} node;
+
+node * create_node(data) {
+
+    node * curr_node = malloc(sizeof (node));
+    
+    curr_node->data = data;
+    curr_node->left = NULL;
+    curr_node->right = NULL;
+    
+    return curr_node;
+}
+
+void bfs(node *root)
+{
+     if(root == NULL)
+     {
+         return;
+     }
+     enqueue(root->left);
+     enqueue(root->right);
+     printf("%d ", root->data);
+     root = (node *) dequeue();
+     bfs(root);
+}
+
+node *insert(node *root, int data) {
+  
+    if(root == NULL)
+    {
+       node * curr_node = create_node(data);
+       root = curr_node;
+    }
+    else if (data > root->data)
+    {
+        root->right = insert(root->right, data);
+    }
+    else
+    {
+        root->left = insert(root->left, data);
+    }
+    return root;
+}
+
+/* Driver program to test above functions*/
+int main()
+{
+   /*            1
+                / \
+               /   \
+              2     3
+             / \
+            /   \
+           4     5
+    */
+
+    node *root = NULL;
+
+    init_queue(sizeof(node));
+
+    root = create_node(1);
+    root->left        = create_node(2);
+    root->right       = create_node(3);
+    root->left->left  = create_node(4);
+    root->left->right = create_node(5);
+ 
+    printf("Expected: 1 2 3 4 5 \n");
+    printf("Level Order traversal of binary tree is \n");
+    bfs(root);
+    printf("\n");
+ 
+    return 0;
+}
---
+++ trees/queue.c
@@ -0,0 +1,106 @@
+//Implement queue as needed for BFS
+//http://www.geeksforgeeks.org/queue-set-1introduction-and-array-implementation/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 20
+
+void** queue;// arr of pointers
+int enqueue_point = -1, dequeue_point = -1, size = -1;
+
+int is_empty()
+{
+    if (enqueue_point == -1) {
+        return 1;
+    }
+    else 
+    {
+        return 0;
+    }
+}
+
+int is_full()
+{
+    if (dequeue_point == N) {
+        return 1;
+    }
+    else 
+    {
+        return 0;
+    }
+}
+
+//pre-increment queue index
+void enqueue(void* data)
+{
+    if(is_full()) 
+    {
+        printf("%s: Queue is full\n", __FUNCTION__);
+	return;
+    }
+    enqueue_point++;
+    queue[enqueue_point] = data;
+}
+
+//post-decrement queue index
+void* dequeue()
+{
+    if(is_empty())
+    {
+        printf("%s: Queue is empty\n", __FUNCTION__);
+	return NULL;
+    }
+    dequeue_point++;
+    return queue[dequeue_point];
+}
+
+void init_queue(int pointer_size ) //size is size of each element in the queue
+{
+    int i;
+
+    queue = malloc(pointer_size * N);
+    for(i = 0; i <N; i++)
+    {
+        queue[i] = malloc(sizeof (pointer_size));
+    }
+}
+
+#if 0
+// Driver program to test above functions./
+int main()
+{
+    float arr[] = {10, 20, 30, 40};
+    float data;
+
+    //int arr[] = {10, 20, 30, 40};
+    //int data;
+
+    init_queue(sizeof(arr[0])); //here int is our element in the queue.
+
+    enqueue(&arr[0]);
+    enqueue(&arr[1]);
+    enqueue(&arr[2]);
+    enqueue(&arr[3]);
+    
+    data = * ((float *) dequeue());
+    printf("%f dequeued from queue\n", data);
+ 
+    data = * ((float *) dequeue());
+    printf("%f dequeued from queue\n", data);
+ 
+    data = * ((float *) dequeue());
+    printf("%f dequeued from queue\n", data);
+ 
+    data = * ((float *) dequeue());
+    printf("%f dequeued from queue\n", data);
+ 
+    data = * ((float *) dequeue());
+    printf("%f dequeued from queue\n", data);
+ 
+    data = * ((float *) dequeue());
+    printf("%f dequeued from queue\n", data);
+ 
+    return 0;
+}
+#endif
